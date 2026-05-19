bff40a167a733d079b0a6bd2c7685351835662a1
daniloobr <devdanilobr@gmail.com>
2026-05-19 00:32:18 -0300

    chore: update application port from 5000 to 5005 in main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     Dica: Em ambiente real de internet, prefira usar Gunicorn ou Waitress.
     """
     print("🚀 Dnex IA está decolando!")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5005, debug=True)
